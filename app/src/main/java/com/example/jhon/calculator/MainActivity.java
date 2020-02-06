83a97cdf919dcd0a9da701a4e0714c10e100c024
Jhon <1723368058@qq.com>
2020-02-06 16:33:26 +0800

    Finished

--- app/src/main/java/com/example/jhon/calculator/MainActivity.java
+++ app/src/main/java/com/example/jhon/calculator/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.jhon.calculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView begin=(ImageView)findViewById(R.id.begin);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        begin.setVisibility(View.GONE);
+                    }
+                });
+            }
+        }).start();
     }
 }
